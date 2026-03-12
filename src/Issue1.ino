2040c9f82e8cb4e2a977ba4bdf80852123f740cd
anicand <alexnicasi2@gmail.com>
2026-03-12 10:45:10 +0100

    Update Issue1.ino

--- src/Issue1.ino
+++ src/Issue1.ino
@@ -1,1 +1,18 @@
+const int pinLDR = 2; 
 
+void setup() {
+  Serial.begin(115200);   // Inicialitzem la comunicació sèrie a 115200 baudios
+  delay(1000);
+  Serial.println("Llegint dades del sensor LDR...");
+}
+
+void loop() {
+   int valor = analogRead(pinLDR);   // Llegim el valor analògic (de 0 a 4095)
+
+  Serial.print("Llum detectada: ");   // Mostra el valor pel Monitor Sèrie
+
+  Serial.println(valor);
+
+  delay(2000);  // Esperem 2000ms (2 segons) per no col·lapsar la pantalla
+
+}
